11362171365470a684640558ae2843f8dc854274
Rodrigo Luiz <79553015+rodrigoLui@users.noreply.github.com>
2021-08-04 22:49:47 -0300

    concluído com sucesso

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+
+let boxLeft = 200
+let boxTop = 200
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    const box = document.getElementById('box')
+    
+
+    switch (keyName) {
+        case 'ArrowLeft':
+            boxLeft-=10
+            box.style.left = `${boxLeft}px`
+            document.getElementById('ratoM').style.transform = 'rotate(90deg)'
+        break;
+
+        case 'ArrowRight':
+            boxLeft+=10
+            box.style.left = `${boxLeft}px`
+            document.getElementById('ratoM').style.transform = 'rotate(-90deg)'
+
+        break;
+
+        case 'ArrowUp':
+                boxTop-=10
+                box.style.top = `${boxTop}px`
+            document.getElementById('ratoM').style.transform = 'rotate(180deg)'
+
+        break;
+
+        case 'ArrowDown':
+                boxTop+=10
+                box.style.top = `${boxTop}px`
+            document.getElementById('ratoM').style.transform = 'rotate(0deg)'
+
+        break;
+
+        default:
+            break;
+    }
+
+    console.log('keydown event\n\n' + 'key: ' + keyName);
+
+
+
+});
